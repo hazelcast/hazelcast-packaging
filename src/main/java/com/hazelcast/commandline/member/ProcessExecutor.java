fd437ddfa18fe25402d5534255b3aa31e2eae54a
alparslanavci <alparslanavci@gmail.com>
2019-05-17 11:55:20 +0100

    Typo fix

--- src/main/java/com/hazelcast/commandline/member/ProcessExecutor.java
+++ src/main/java/com/hazelcast/commandline/member/ProcessExecutor.java
@@ -23,7 +23,7 @@ import java.lang.reflect.Field;
 import java.util.List;
 
 /**
- * Hanlder for OS level process operations.
+ * Handler for OS level process operations.
  */
 public class ProcessExecutor {
     Process buildAndStart(List<String> commandList, boolean foreground)
