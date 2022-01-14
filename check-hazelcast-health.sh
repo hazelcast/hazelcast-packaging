822474b05c2110d5c51ab7a5803514f7f5e97205
Frantisek Hartman <frant.hartm@gmail.com>
2022-01-14 17:59:10 +0100

    Cat logs

--- check-hazelcast-health.sh
+++ check-hazelcast-health.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 attempts=0
-max_attempts=10
+max_attempts=30
 until $(curl --silent --fail "127.0.0.1:5701/hazelcast/health/ready"); do
   if [ ${attempts} -eq ${max_attempts} ];then
       echo "Hazelcast not responding"
+      cat hz.log
       exit 1
   fi
   printf '.'
