9b73654d90dc1fe1313333f7a4911b1b58a28cd1
Frantisek Hartman <frant.hartm@gmail.com>
2022-01-14 10:00:20 +0100

    Use hazelcast/homebrew-hz-test repo

--- common.sh
+++ common.sh
@@ -23,9 +23,8 @@ if [ "${EVENT_NAME}" == "pull_request" ]; then
   export DEBIAN_REPO_BASE_URL="https://repository.hazelcast.com/debian-test-local"
   export RPM_REPO_BASE_URL="https://repository.hazelcast.com/rpm-test-local"
 
-  # TODO change when the access is setup
-  export BREW_GIT_REPO_NAME="frant-hartm/homebrew-hz"
-  export BREW_REPO_NAME="frant-hartm/hz"
+  export BREW_GIT_REPO_NAME="hazelcast/homebrew-hz-test"
+  export BREW_REPO_NAME="hazelcast/hz-test"
 else
   export DEBIAN_REPO_BASE_URL="https://repository.hazelcast.com/debian-local"
   export RPM_REPO_BASE_URL="https://repository.hazelcast.com/rpm-local"
