9f7a00ab43452ca143874d96ea0981914e46dbb9
Frantisek Hartman <frant.hartm@gmail.com>
2022-01-28 15:15:50 +0100

    Update readme and add few comments

--- common.sh
+++ common.sh
@@ -13,18 +13,22 @@ if [[ "$HZ_VERSION" == *"BETA"* ]]; then
   export PACKAGE_REPO=beta
 fi
 
+# For brew the version is lowercase and with `.` instead of `-`
 BREW_PACKAGE_VERSION=$(echo $PACKAGE_VERSION | tr '[:upper:]' '[:lower:]' | sed -r -r 's/(-)/\./g')
 export BREW_PACKAGE_VERSION
 
+# For RPM the version is with `.` instead of `-`
 RPM_PACKAGE_VERSION=$(echo $PACKAGE_VERSION | sed -r -r 's/(-)/\./g')
 export RPM_PACKAGE_VERSION
 
 if [ "${EVENT_NAME}" == "pull_request" ]; then
+  # PRs publish to test repositories and install the packages from there
   export DEBIAN_REPO=debian-test-local
   export DEBIAN_REPO_BASE_URL="https://repository.hazelcast.com/${DEBIAN_REPO}"
   export RPM_REPO=rpm-test-local
   export RPM_REPO_BASE_URL="https://repository.hazelcast.com/${RPM_REPO}"
 
+  # This is a clone of the hazelcast/homebrew-hz repository
   export BREW_GIT_REPO_NAME="hazelcast/homebrew-hz-test"
   export BREW_TAP_NAME="hazelcast/hz-test"
 else
