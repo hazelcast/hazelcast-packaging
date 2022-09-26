62486b8d59276ccf775e5face03bdb542209c5ab
Tomasz Gawęda <tomasz.gaweda@outlook.com>
2022-09-26 17:43:46 +0200

    Merge files, parameterize scripts

--- vagrant/fedora/installRepo.sh
+++ vagrant/fedora/installRepo.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
 # note: fedora has no wget by default (strange, yeah), so it's additional query beside those mentioned in repo
 sudo yum -y install wget
-wget https://repository.hazelcast.com/rpm/stable/hazelcast-rpm-stable.repo -O hazelcast-rpm-stable.repo
-sudo mv hazelcast-rpm-stable.repo /etc/yum.repos.d/
+
+local packageVersion='stable'
+if [ -z "${PACKAGE_VERSION}" ]; then
+  packageVersion="$PACKAGE_VERSION"
+fi
+wget https://repository.hazelcast.com/rpm/stable/hazelcast-rpm-${packageVersion}.repo -O hazelcast-rpm-${packageVersion}.repo
+sudo mv hazelcast-rpm-${packageVersion}.repo /etc/yum.repos.d/
