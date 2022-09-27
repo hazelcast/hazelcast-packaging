b67f535c63b24edb04ab1a7169bc8044d4862b17
Tomasz Gawęda <tomasz.gaweda@outlook.com>
2022-09-27 16:44:36 +0200

    Repository name configurable

--- vagrant/fedora/installRepo.sh
+++ vagrant/fedora/installRepo.sh
@@ -3,9 +3,11 @@
 # note: fedora has no wget by default (strange, yeah), so it's additional query beside those mentioned in repo
 sudo yum -y install wget
 
+local repoName="$1"
+
 local packageVersion='stable'
-if [ "$1" ]; then
-  packageVersion="$1"
+if [ "$2" ]; then
+  packageVersion="$2"
 fi
-wget https://repository.hazelcast.com/rpm/${packageVersion}/hazelcast-rpm-${packageVersion}.repo -O hazelcast-rpm-${packageVersion}.repo
+wget https://repository.hazelcast.com/${repoName}/${packageVersion}/hazelcast-rpm-${packageVersion}.repo -O hazelcast-rpm-${packageVersion}.repo
 sudo mv hazelcast-rpm-${packageVersion}.repo /etc/yum.repos.d/
