c3d8af927327cc1e0b17fdb21a6469a007fd326b
Tomasz Gawęda <tomasz.gaweda@outlook.com>
2022-09-28 12:57:04 +0200

    Cleanups

--- check-hazelcast-health.sh
+++ check-hazelcast-health.sh
@@ -5,7 +5,7 @@ max_attempts=30
 until $(curl --silent --fail "127.0.0.1:5701/hazelcast/health/ready"); do
   if [ ${attempts} -eq ${max_attempts} ];then
       echo "Hazelcast not responding"
-      cat ~/hz.log
+      cat hz.log
       ps aux
       exit 1
   fi
