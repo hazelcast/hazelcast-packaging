6ea306b96f6062d6522a80c0feb8f4e1dd954cf2
Łukasz Dziedziul <lukasz.dziedziul@hazelcast.com>
2024-05-20 18:45:53 +0200

    Use .github/release_type for controlling which editions are released

--- .github/workflows/build.functions.sh
+++ .github/workflows/build.functions.sh
@@ -2,105 +2,43 @@
 
 set -euo pipefail
 
-function get_patch_part() {
-  local version=$1
-  local patch_part=$(echo "$version" | awk -F'.' '{print $3}')
-  echo "$patch_part"
-}
-
-function is_numeric() {
-  local str=$1
-  [[ $str =~ ^[0-9]+$ ]]
-}
-
-function assert_same_minor_version() {
-  local oss_version=$1
-  local ee_version=$2
-
-  local oss_xy_part=$(echo "$oss_version" | awk -F'.' '{print $1 "." $2}')
-  local ee_xy_part=$(echo "$ee_version" | awk -F'.' '{print $1 "." $2}')
-
-  if [[ $oss_xy_part != $ee_xy_part ]]; then
-    echo "Error: OSS and EE version must have same minor version"
-    exit 1
-  fi
-}
-
-# Checks if we should build the OSS artifacts.
-# Returns "yes" if we should build it or "no" if we shouldn't.
-#
-# - If the workflows was triggered by `pull_request` OSS artifact should be built always
-#
-# If the workflow was triggered by "push" (usually tag push) we should build an OSS artifacts only with these conditions fulfilled:
-#  - both (OSS and EE) HZ versions are final release (non-BETA/DEVEL/SNAPSHOT)
-#  - both (OSS and EE) HZ versions are equal
-#
-# The reasoning:
-# - For minor releases we will have the same version set (OSS=`6.6.0` and EE=`6.6.0`) which means
-# we should build both editions.
-#
-# - For patch releases we will increment only EE version (i.e. OSS="6.6.0" and EE=`6.6.1`) which means we should build EE only
-#
-# - If for some reason we will have to build an emergency OSS patch release we should change OSS version
-# as well (i.e. OSS="6.6.1" and EE=`6.6.1`) this will mean we should build both editions
-#
-# - If the workflows was triggered manually by `workflow_dispatch` we assume that the caller knows what they're doing
-# so we return "yes" for "All" and "OSS" editions
-#
-# Check test cases in `build.functions_tests.sh` to see the examples
+# Checks if we should build the OSS docker image.
+# If the workflow was triggered by `pull_request` OSS artifact should be built always.
+# For other workflows we should build EE only for 'ALL' and 'OSS' editions.
 function should_build_oss() {
 
-  local oss_version=$1
-  local ee_version=$2
-  local triggered_by=$3
-  local editions=$4
-
-  assert_same_minor_version "$oss_version" "$ee_version"
-
-  if [[ $triggered_by == "pull_request" ]]; then
-    echo "yes"
-    return
-  fi
-
-  if [[ $triggered_by == "workflow_dispatch" ]]; then
-    if [[ $editions == "All" || $editions == "OSS" ]]; then
-      echo "yes"
-      return
-    fi
-  fi
-
-  if [[ $triggered_by == "push" ]]; then
-    local oss_patch_part=$(get_patch_part "$oss_version")
-    local ee_patch_part=$(get_patch_part "$ee_version")
-
-    if is_numeric "$oss_patch_part" && [[ "$oss_patch_part" == "$ee_patch_part" ]]; then
-      echo "yes"
-      return
-    fi
-  fi
-
-  echo "no"
-}
-
-# Checks if we should build the EE artifacts.
-# Returns "yes" if we should build it or "no" if we shouldn't.
-#
-# - If the workflows was triggered by `pull_request` EE artifact should be built always
-# - For other workflows we should build EE only for 'All' and 'EE' editions
-#
-function should_build_ee() {
   local triggered_by=$1
-  local editions=$2
+  local release_type=$2
 
   if [[ $triggered_by == "pull_request" ]]; then
     echo "yes"
     return
   fi
 
-  if [[ $editions == "All" || $editions == "EE" ]]; then
+  if [[ $release_type == "ALL" || $release_type == "OSS" ]]; then
+    echo "yes"
+  else
+    echo "no"
+  fi
+}
+
+# Checks if we should build the EE docker image.
+# If the workflow was triggered by `pull_request` EE artifact should be built always.
+# For other workflows we should build EE only for 'All' and 'EE' editions.
+function should_build_ee() {
+
+  local triggered_by=$1
+  local release_type=$2
+
+  if [[ $triggered_by == "pull_request" ]]; then
     echo "yes"
     return
   fi
 
-  echo "no"
+  if [[ $release_type == "ALL" || $release_type == "EE" ]]; then
+    echo "yes"
+  else
+    echo "no"
+  fi
 }
+
