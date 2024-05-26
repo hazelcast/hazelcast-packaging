54eed303fb06e7e50973444edc0f52cbe39db815
Łukasz Dziedziul <lukasz.dziedziul@hazelcast.com>
2024-05-26 23:10:43 +0200

    Use .github/release_type for controlling releases of the OSS/EE [DI-52] (#204)
    In order to control which editions (OSS/EE/ALL) should be released
    `.github/release_type` file is introduced.
    
    This file is required now for newly created git tags. It should contains
    only one of the following values:
    - OSS
    - EE
    - ALL
    
    When a new tag is pushed we read the file to decide which editions
    should be released.
    For manual runs of the `publish-packages.yml` we ignore this file and
    use `RELEASE_TYPE` input from the workflow
    
    `.github/release_type` should be added during the release by the
    automation. It's forbidden to add it master or maintenance branches
    through PRs.
    
    Fixes: https://hazelcast.atlassian.net/browse/DI-52
    
    ---------
    
    Co-authored-by: Jack Green <jack.green@hazelcast.com>

---
+++ .github/workflows/build.functions_tests.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+set -eu
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
+. "$SCRIPT_DIR"/assert.sh/assert.sh
+. "$SCRIPT_DIR"/build.functions.sh
+
+TESTS_RESULT=0
+
+function assert_should_build_oss {
+  local triggered_by=$1
+  local release_type=$2
+  local expected_should_build_os=$3
+  local actual=$(should_build_oss "$triggered_by" "$release_type")
+  assert_eq "$expected_should_build_os" "$actual" "For triggered_by=$triggered_by release_type=$release_type \
+we should$( [ "$expected_should_build_os" = "no" ] && echo " NOT") build OS" || TESTS_RESULT=$?
+}
+
+log_header "Tests for should_build_oss"
+assert_should_build_oss "push" "ALL" "yes"
+assert_should_build_oss "push" "OSS" "yes"
+assert_should_build_oss "push" "EE" "no"
+assert_should_build_oss "workflow_dispatch" "ALL" "yes"
+assert_should_build_oss "workflow_dispatch" "OSS" "yes"
+assert_should_build_oss "workflow_dispatch" "EE" "no"
+assert_should_build_oss "pull_request" "ALL" "yes"
+assert_should_build_oss "pull_request" "OSS" "yes"
+assert_should_build_oss "pull_request" "EE" "yes"
+
+function assert_should_build_ee {
+  local triggered_by=$1
+  local release_type=$2
+  local expected_should_build_ee=$3
+  local actual=$(should_build_ee "$triggered_by" "$release_type")
+  assert_eq "$expected_should_build_ee" "$actual" "For triggered_by=$triggered_by release_type=$release_type \
+we should$( [ "$expected_should_build_ee" = "no" ] && echo " NOT") build EE" || TESTS_RESULT=$?
+}
+
+log_header "Tests for should_build_ee"
+assert_should_build_ee "push" "ALL" "yes"
+assert_should_build_ee "push" "OSS" "no"
+assert_should_build_ee "push" "EE" "yes"
+assert_should_build_ee "workflow_dispatch" "ALL" "yes"
+assert_should_build_ee "workflow_dispatch" "OSS" "no"
+assert_should_build_ee "workflow_dispatch" "EE" "yes"
+assert_should_build_ee "pull_request" "ALL" "yes"
+assert_should_build_ee "pull_request" "OSS" "yes"
+assert_should_build_ee "pull_request" "EE" "yes"
+
+assert_eq 0 "$TESTS_RESULT" "ALL tests should pass"
