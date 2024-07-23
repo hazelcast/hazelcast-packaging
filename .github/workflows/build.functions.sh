99c60bcf60ad6eda2a6727a4ac88b6613106758f
Łukasz Dziedziul <lukasz.dziedziul@hazelcast.com>
2024-07-23 14:35:01 +0200

    Use internal snapshot repository for OSS snapshot packages [5.5.z]

--- .github/workflows/build.functions.sh
+++ .github/workflows/build.functions.sh
@@ -32,3 +32,36 @@ function should_build_ee() {
   fi
 }
 
+function get_hz_dist_tar_gz() {
+  local hz_version=$1
+  local distribution=$2
+  local extension=tar.gz
+  local url
+
+  if [[ "$distribution" == "hazelcast" ]]; then
+    if [[ "${hz_version}" == *"SNAPSHOT"* ]]; then
+      url="https://${HZ_SNAPSHOT_INTERNAL_USERNAME}:${HZ_SNAPSHOT_INTERNAL_PASSWORD}@repository.hazelcast.com/snapshot-internal/com/hazelcast/hazelcast-distribution/${hz_version}/hazelcast-distribution-${hz_version}.$extension"
+    else
+      url="https://repo1.maven.org/maven2/com/hazelcast/hazelcast-distribution/${hz_version}/hazelcast-distribution-${hz_version}.$extension"
+    fi
+  elif [[ "$distribution" == "hazelcast-enterprise" ]]; then
+    local repository
+    if [[ "${hz_version}" == *"SNAPSHOT"* ]]; then
+      repository=snapshot
+    else
+      repository=release
+    fi
+    url="https://repository.hazelcast.com/${repository}/com/hazelcast/hazelcast-enterprise-distribution/${hz_version}/hazelcast-enterprise-distribution-${hz_version}.$extension"
+  fi
+  echo "$url"
+}
+
+function url_contains_password() {
+  local url=$1
+  local password=$2
+  if [[ "$url" == *"$password"* ]]; then
+    echo "yes"
+  else
+    echo "no"
+  fi
+}
