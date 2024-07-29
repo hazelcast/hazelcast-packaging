2ec482f0dab9c0e24a97b2547a517ca6eaeedde1
Jack <jack.green@hazelcast.com>
2024-07-29 16:27:03 +0100

    Extend package validation tests as per `hazelcast-docker` [DI-215]
    In https://github.com/hazelcast/hazelcast-docker/pull/776, additional checks were added to `hazelcast-docker`'s tests to assert variant and version.
    
    Rather than duplicate these tests, here, instead refactored those tests so that the logic can be shared - https://github.com/hazelcast/hazelcast-docker/pull/790
    
    This PR adapts these tests to use this framework, giving additional validation:
    - variant and version
    - `CLC` can connect to `IMap` in instance

--- check-hazelcast-health.sh
+++ check-hazelcast-health.sh
@@ -1,15 +1,31 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-attempts=0
-max_attempts=30
-until $(curl --silent --fail "127.0.0.1:5701/hazelcast/health/ready"); do
-  if [ ${attempts} -eq ${max_attempts} ];then
-      echo "Hazelcast not responding"
-      cat hz.log
-      ps -aux
-      exit 1
-  fi
-  printf '.'
-  attempts=$(($attempts+1))
-  sleep 2
-done
+set -o errexit
+
+# Source the latest version of `abstract-simple-smoke-test.sh` from the `hazelcast-docker` repository and include in current shell
+# TODO Use `master` once merged
+abstract_simple_smoke_test_script_content=$(curl --silent https://raw.githubusercontent.com/hazelcast/hazelcast-docker/DI-215-Add-additional-Deb/RPM/Brew-tests/.github/scripts/abstract-simple-smoke-test.sh)
+# shellcheck source=/dev/null
+. <(echo "${abstract_simple_smoke_test_script_content}")
+
+function get_logs() {
+    cat hz.log
+}
+
+input_distribution_type=$1
+expected_version=$2
+
+case "${input_distribution_type}" in
+  "hazelcast")
+    expected_distribution_type="Hazelcast Platform"
+    ;;
+  "hazelcast-enterprise")
+    expected_distribution_type="Hazelcast Enterprise"
+    ;;
+  *)
+    echoerr "Unrecognized distribution type ${input_distribution_type}"
+    exit 1
+    ;;
+esac
+
+test_package "${expected_distribution_type}" "${expected_version}"
