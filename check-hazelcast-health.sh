e9ca6a11a316d838360ccb028dda4c90f5293660
Jack <jack.green@hazelcast.com>
2024-08-01 12:03:34 +0100

    Move `derive_expected_distribution_type` to function

--- check-hazelcast-health.sh
+++ check-hazelcast-health.sh
@@ -18,20 +18,25 @@ function get_hz_logs() {
     cat hz.log
 }
 
+function derive_expected_distribution_type() {
+  local input_distribution_type=$1
+
+  case "${input_distribution_type}" in
+    "hazelcast")
+      echo "Hazelcast Platform"
+      ;;
+    "hazelcast-enterprise")
+      echo "Hazelcast Enterprise"
+      ;;
+    *)
+      echoerr "Unrecognized distribution type ${input_distribution_type}"
+      exit 1
+      ;;
+  esac
+}
+
 input_distribution_type=$1
 expected_version=$2
 
-case "${input_distribution_type}" in
-  "hazelcast")
-    expected_distribution_type="Hazelcast Platform"
-    ;;
-  "hazelcast-enterprise")
-    expected_distribution_type="Hazelcast Enterprise"
-    ;;
-  *)
-    echoerr "Unrecognized distribution type ${input_distribution_type}"
-    exit 1
-    ;;
-esac
-
+expected_distribution_type=$(derive_expected_distribution_type "${input_distribution_type}")
 test_package "${expected_distribution_type}" "${expected_version}"
