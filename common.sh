16298e1668e75245462b535c1f9ccd0998434168
Jack Green <jack.green@hazelcast.com>
2024-08-14 14:47:00 +0100

    Fix private test repository access [DI-236] (#221)
    In [DI-222](https://hazelcast.atlassian.net/browse/DI-222) the test
    repositories were made private, but that caused [build
    failures](https://github.com/hazelcast/hazelcast-packaging/actions/runs/10365877156/job/28693937495).
    
    While the _initial_ connection to the repositories to get metadata was
    authenticated, the ultimate `install` was not and failed.
    
    Fixed by adding the credentials to the URL as advised by [JFrog
    documentation](https://jfrog.com/help/r/jfrog-artifactory-documentation/configure-authenticated-access-to-debian-servers)
    
    Fixes: [DI-236](https://hazelcast.atlassian.net/browse/DI-236)
    
    [DI-222]:
    https://hazelcast.atlassian.net/browse/DI-222?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    [DI-236]:
    https://hazelcast.atlassian.net/browse/DI-236?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    
    ---------
    
    Co-authored-by: Łukasz Dziedziul <lukasz.dziedziul@hazelcast.com>
    
    (cherry picked from commit d12e3f1338c6f555346e60d795a1522405eb20fe)

--- common.sh
+++ common.sh
@@ -61,9 +61,8 @@ export BREW_PACKAGE_VERSION
 if [ "${USE_TEST_REPO}" == "true" ]; then
   # PRs publish to test repositories and install the packages from there
   export DEBIAN_REPO=debian-test-local
-  export DEBIAN_REPO_BASE_URL="https://repository.hazelcast.com/${DEBIAN_REPO}"
+  export DEBIAN_REPO_BASE_URL="https://${JFROG_USERNAME}:${JFROG_TOKEN}@repository.hazelcast.com/${DEBIAN_REPO}"
   export RPM_REPO=rpm-test-local
-  export RPM_REPO_BASE_URL="https://repository.hazelcast.com/${RPM_REPO}"
 
   # This is a clone of the hazelcast/homebrew-hz repository
   export BREW_GIT_REPO_NAME="hazelcast/homebrew-hz-test"
@@ -72,8 +71,10 @@ else
   export DEBIAN_REPO=debian-local
   export DEBIAN_REPO_BASE_URL="https://repository.hazelcast.com/${DEBIAN_REPO}"
   export RPM_REPO=rpm-local
-  export RPM_REPO_BASE_URL="https://repository.hazelcast.com/${RPM_REPO}"
 
   export BREW_GIT_REPO_NAME="hazelcast/homebrew-hz"
   export BREW_TAP_NAME="hazelcast/hz"
 fi
+
+export RPM_REPO_BASE_URL="https://repository.hazelcast.com/${RPM_REPO}"
+export HZ_DISTRIBUTION_FILE=distribution.tar.gz
