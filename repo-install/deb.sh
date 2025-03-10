7fef9acdec45c29994ad75e88980c21bc515d02b
Tomasz Gawęda <tomasz.gaweda@outlook.com>
2025-03-10 19:03:49 +0100

    Update script

--- repo-install/deb.sh
+++ repo-install/deb.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+branch="${1:-stable}"
+version="${2:-main}"
+
 # install tools used: wget, gpg, tee (coreutils)
 # do not upgrade if already installed
-sudo apt install --no-upgrade wget gpg coreutils
+sudo apt-get install -y --no-upgrade wget gpg coreutils
 
 wget -qO - https://repository.hazelcast.com/api/gpg/key/public | gpg --dearmor | sudo tee /usr/share/keyrings/hazelcast-archive-keyring.gpg > /dev/null
-echo "deb [signed-by=/usr/share/keyrings/hazelcast-archive-keyring.gpg] https://repository.hazelcast.com/debian stable main" | sudo tee -a /etc/apt/sources.list
-sudo apt update
+echo "deb [signed-by=/usr/share/keyrings/hazelcast-archive-keyring.gpg] https://repository.hazelcast.com/debian $branch $version" | sudo tee -a /etc/apt/sources.list
+sudo apt-get update
