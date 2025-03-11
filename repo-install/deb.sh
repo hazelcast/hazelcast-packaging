661c8686c32660f2a462b21a05adde9e75c04375
Tomasz Gawęda <tomasz.gaweda@outlook.com>
2025-03-11 13:01:44 +0100

    Update repo-install/deb.sh
    Co-authored-by: Jack Green <jack.green@hazelcast.com>

--- repo-install/deb.sh
+++ repo-install/deb.sh
@@ -5,7 +5,7 @@ version="${2:-main}"
 
 # install tools used: wget, gpg, tee (coreutils)
 # do not upgrade if already installed
-sudo apt-get install -y --no-upgrade wget gpg coreutils
+sudo apt-get install --assume-yes --no-upgrade wget gpg coreutils
 
 wget -qO - https://repository.hazelcast.com/api/gpg/key/public | gpg --dearmor | sudo tee /usr/share/keyrings/hazelcast-archive-keyring.gpg > /dev/null
 echo "deb [signed-by=/usr/share/keyrings/hazelcast-archive-keyring.gpg] https://repository.hazelcast.com/debian ${branch} ${version}" | sudo tee -a /etc/apt/sources.list
