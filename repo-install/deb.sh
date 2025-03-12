3756321ad2ae31ee41a422d26b4412b9e6cee838
Tomasz Gawęda <tomasz.gaweda@outlook.com>
2025-03-12 13:59:53 +0100

    Workaround for no sudo

--- repo-install/deb.sh
+++ repo-install/deb.sh
@@ -3,10 +3,16 @@
 branch="${1:-stable}"
 version="${2:-main}"
 
+# docker doesn't have sudo - it's running as root by default
+SUDO=""
+if (( $EUID != 0 )); then
+    SUDO="sudo"
+fi
+
 # install tools used: wget, gpg, tee (coreutils)
 # do not upgrade if already installed
-sudo apt-get install --assume-yes --no-upgrade wget gpg coreutils
+$SUDO apt-get install --assume-yes --no-upgrade wget gpg coreutils
 
-wget -qO - https://repository.hazelcast.com/api/gpg/key/public | gpg --dearmor | sudo tee /usr/share/keyrings/hazelcast-archive-keyring.gpg > /dev/null
-echo "deb [signed-by=/usr/share/keyrings/hazelcast-archive-keyring.gpg] https://repository.hazelcast.com/debian ${branch} ${version}" | sudo tee -a /etc/apt/sources.list
-sudo apt-get update
+wget -qO - https://repository.hazelcast.com/api/gpg/key/public | gpg --dearmor | $SUDO tee /usr/share/keyrings/hazelcast-archive-keyring.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/hazelcast-archive-keyring.gpg] https://repository.hazelcast.com/debian ${branch} ${version}" | $SUDO tee -a /etc/apt/sources.list
+$SUDO apt-get update
