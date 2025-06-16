709a242071da06e91e48c131fdfd02d3c48b87b6
Jack Green <JackPGreen@Gmail.com>
2025-06-16 15:24:51 +0100

    Refactor how `assert.sh` is downloaded for tests
    We can simplify as discussed [here](https://github.com/hazelcast/docker-actions/pull/1#discussion_r2149451062).

--- common_tests.sh
+++ common_tests.sh
@@ -5,10 +5,7 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 export USE_TEST_REPO=true
 
 # Source the latest version of assert.sh unit testing library and include in current shell
-assert_script_content=$(curl --silent https://raw.githubusercontent.com/hazelcast/assert.sh/main/assert.sh)
-# shellcheck source=/dev/null
-. <(echo "${assert_script_content}")
-. "$SCRIPT_DIR"/common.sh
+source /dev/stdin <<< "$(curl --silent https://raw.githubusercontent.com/hazelcast/assert.sh/main/assert.sh)"
 
 TESTS_RESULT=0
 
